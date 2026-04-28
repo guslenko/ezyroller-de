432e4558d7300c84847bdc119485e533184194b3
Olexander Huslenko <guslenko@gmail.com>
2026-04-28 15:41:24 +0200

    v.4.4.7

--- apps/web/app/plugins/gtm.client.ts
+++ apps/web/app/plugins/gtm.client.ts
@@ -1,14 +1,20 @@
+// @ts-nocheck
 export default defineNuxtPlugin(() => {
   if (process.client) {
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-      'gtm.start': new Date().getTime(),
-      event: 'gtm.js'
-    });
+    const id = 'GTM-T24Q79K3' // ← твой новый GTM ID
 
-    const script = document.createElement("script");
-    script.async = true;
-    script.src = "https://www.googletagmanager.com/gtm.js?id=GTM-5SZG99B";
-    document.head.appendChild(script);
+    // GTM script
+    const script = document.createElement('script')
+    script.async = true
+    script.src = `https://www.googletagmanager.com/gtm.js?id=${id}`
+    document.head.appendChild(script)
+
+    // GTM noscript
+    const noscript = document.createElement('noscript')
+    noscript.innerHTML = `
+      <iframe src="https://www.googletagmanager.com/ns.html?id=${id}"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    `
+    document.body.prepend(noscript)
   }
-});
+})
